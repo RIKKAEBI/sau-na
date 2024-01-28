5a7a358c8bd64da854a999aface00c54a86ddfcf
RIKKAEBI <135724197+RIKKAEBI@users.noreply.github.com>
2024-01-28 12:35:14 +0900

    🚧 (WIP)webComponentが本番環境で動く確認

--- controller/home.go
+++ controller/home.go
@@ -7,5 +7,5 @@ import (
 )
 
 func Home(c echo.Context) error {
-	return c.String(http.StatusOK, "hello world!")
+	return c.HTML(http.StatusOK, "<script src='https://unpkg.com/htmx.org@1.9.10' integrity='sha384-D1Kt99CQMDuVetoL1lrYwg5t+9QdHe7NLX/SoJYkXDFfX37iInKRy5xLSi8nO7UC' crossorigin='anonymous'></script><link rel='stylesheet' crossorigin href='/assets/style.css'><script type='module' crossorigin src='/assets/main.js'></script><button hx-post='/api/users/me'>hello world!</button>")
 }
