01f61f55be922a0f2d2b60ee17b871839dae3817
haryoiro <mizusecocolte@gmail.com>
2024-01-28 19:18:40 +0900

    ♻️  renderer周りをリファクタ

--- middleware/renderer.go
+++ middleware/renderer.go
@@ -22,7 +22,7 @@ func (t *TemplateRenderer) Render(w io.Writer, name string, data interface{}, c 
 
 func InitRenderer(e *echo.Echo) {
 	renderer := &TemplateRenderer{
-		templates: template.Must(template.ParseGlob("./public/*.html")),
+		templates: template.Must(template.ParseGlob("./template/*.html")),
 	}
 	e.Renderer = renderer
 }
--- router/router.go
+++ router/router.go
@@ -1,33 +1,14 @@
 package router
 
 import (
-	"html/template"
-	"io"
-
 	"github.com/labstack/echo/v4"
 )
 
-type TemplateRenderer struct {
-	templates *template.Template
-}
-
-// Render renders a template document
-func (t *TemplateRenderer) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
-
-	// Add global methods if data is a map
-	if viewContext, isMap := data.(map[string]interface{}); isMap {
-		viewContext["reverse"] = c.Echo().Reverse
-	}
-
-	return t.templates.ExecuteTemplate(w, name, data)
-}
-
 func InitRouter(e *echo.Echo) {
-	renderer := &TemplateRenderer{
-		templates: template.Must(template.ParseGlob("template/*.html")),
-	}
-	e.Renderer = renderer
+	setStatic(e)
+}
 
+func setStatic(e *echo.Echo) {
 	e.File("/", "./components/dist/index.html")
 	e.File("/favicon.ico", "./components/dist/favicon.ico")
 	e.File("/assets/main.js", "./components/dist/assets/main.js")
