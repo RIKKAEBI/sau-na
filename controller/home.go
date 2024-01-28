44fddee4770e76225b59149505aa7087160e1251
RIKKAEBI <135724197+RIKKAEBI@users.noreply.github.com>
2024-01-28 12:56:54 +0900

    🚧 webComponentを使用してみる

--- controller/home.go
+++ controller/home.go
@@ -7,5 +7,5 @@ import (
 )
 
 func Home(c echo.Context) error {
-	return c.HTML(http.StatusOK, "<script src='https://unpkg.com/htmx.org@1.9.10' integrity='sha384-D1Kt99CQMDuVetoL1lrYwg5t+9QdHe7NLX/SoJYkXDFfX37iInKRy5xLSi8nO7UC' crossorigin='anonymous'></script><link rel='stylesheet' crossorigin href='/assets/style.css'><script type='module' crossorigin src='/assets/main.js'></script><button hx-post='/api/users/me'>hello world!</button>")
+	return c.HTML(http.StatusOK, "<script src='https://unpkg.com/htmx.org@1.9.10' integrity='sha384-D1Kt99CQMDuVetoL1lrYwg5t+9QdHe7NLX/SoJYkXDFfX37iInKRy5xLSi8nO7UC' crossorigin='anonymous'></script><link rel='stylesheet' crossorigin href='/assets/style.css'><script type='module' crossorigin src='/assets/main.js'></script><sau-button hx-post='/api/users/me'>hello world!</sau-button><br><sau-button outline hx-post='/api/users/me'>クリックして！！</sau-button>")
 }
