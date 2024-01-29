5cde9a9589d950ae4cdbf2d095cb0c7d41fddfbb
RIKKAEBI <135724197+RIKKAEBI@users.noreply.github.com>
2024-01-29 14:27:29 +0000

    🚧 一旦適当なページに飛ばすように

--- components/src/components/LoginButton.tsx
+++ components/src/components/LoginButton.tsx
@@ -1,12 +1,13 @@
 import { JSX } from "solid-js"
 import logo from "../assets/web_light_rd_SI@4x.png"
+import { ExtButton } from "./Button"
 
 export const LoginButton = (
-  props: JSX.ButtonHTMLAttributes<HTMLButtonElement>,
+  props: JSX.ButtonHTMLAttributes<HTMLAnchorElement>,
 ) => {
   return (
-    <button {...props}>
+    <ExtButton path="/register" {...props}>
       <img src={logo} alt="google" />
-    </button>
+    </ExtButton>
   )
 }
