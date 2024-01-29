ba7a3b2597c04f498aab12efa0210b8efcebbff4
RIKKAEBI <135724197+RIKKAEBI@users.noreply.github.com>
2024-01-29 11:35:53 +0000

    👍 prettierの導入

--- components/src/App.tsx
+++ components/src/App.tsx
@@ -1,12 +1,12 @@
-import { createSignal } from 'solid-js'
-import './App.css'
+import { createSignal } from "solid-js"
+import "./App.css"
 
 function App() {
   const [count, setCount] = createSignal(0)
 
   return (
     <>
-      <h1>さう〜な</h1>    
+      <h1>さう〜な</h1>
       <button onClick={() => setCount((count) => count + 1)}>
         カウンター {count()}
       </button>
