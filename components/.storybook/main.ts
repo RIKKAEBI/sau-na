1491e7ebc531912ac88216ab6f9911f54456ee15
RIKKAEBI <135724197+RIKKAEBI@users.noreply.github.com>
2024-02-06 15:01:22 +0000

    👍 storybookのプロジェクトの初期化

---
+++ components/.storybook/main.ts
@@ -0,0 +1,23 @@
+import type { StorybookConfig } from "storybook-solidjs-vite"
+
+const config: StorybookConfig = {
+  stories: [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+    "./page/*.mdx",
+  ],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/addon-styling-webpack",
+  ],
+  framework: {
+    name: "storybook-solidjs-vite",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+}
+export default config
---
+++ components/src/components/Button.stories.tsx
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from "storybook-solidjs"
+import { Button } from "./Button"
+
+const meta = {
+  title: "atom/Button",
+  component: Button,
+  args: {
+    children: "クリックして！",
+    outline: true,
+  },
+} satisfies Meta<typeof Button>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Primary: Story = {
+  args: {},
+}
